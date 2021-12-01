6526fb9fd7176671eeb0de262a601234f09d03da
Sahib Singh <75523820+ssahibsingh@users.noreply.github.com>
2021-12-01 13:47:30 +0530

    Added users.sql

---
+++ Database/users.sql
@@ -0,0 +1,11 @@
+INSERT INTO users(username, profile_photo_url, bio) VALUES 
+("kanavphull","https://pbs.twimg.com/profile_images/1386885117428191232/70SyHOxP_400x400.jpg","Hedonist yet Spiritual || IT at NITJ 2024"),
+('raj gupta' , '/klsadf893724:f//432' , 'soon to be updated'),
+('Sahib Singh' , 'https://pbs.twimg.com/profile_images/1465003815820693506/gbTJoe66_400x400.jpg' ,"Life is a journey, It drives you MAD.|| IT NITJ'24"),
+("sakshi" , "/irhe734//0/32" , "NITJ wish me on 23 jan"),
+("chhaprinibba", "/sdfvsdf", "Heart Stealer"),
+("dettol sharma", "/sdfvsdf", "Dettol Stealer"),
+('sunil' , '/yisadf324//' , 'hotel manageemnt '),
+('sanjay' , '/fduiahj43' , 'football lover' ),
+('Axel Sivert Anker' , '/adaskjnas','Norwegian'),
+('Steven','/acdsccsdc', 'living life my way');

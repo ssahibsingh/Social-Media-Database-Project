0905ec71f3a1db3691d1c568db12f20205a8b03e
Sahib Singh <75523820+ssahibsingh@users.noreply.github.com>
2021-12-02 10:07:59 +0530

    Corrected Follow Database

--- Database/users.sql
+++ Database/users.sql
@@ -1,7 +1,7 @@
 
 INSERT INTO users(username, profile_photo_url, bio) VALUES 
 ("kanavphull","https://pbs.twimg.com/profile_images/1386885117428191232/70SyHOxP_400x400.jpg","Hedonist yet Spiritual || IT at NITJ 2024"),
-('raj gupta' , '/klsadf893724:f//432' , 'soon to be updated'),
+('Raj Gupta' , '/klsadf893724:f//432' , 'soon to be updated'),
 ('Sahib Singh' , 'https://pbs.twimg.com/profile_images/1465003815820693506/gbTJoe66_400x400.jpg' ,"Life is a journey, It drives you MAD.|| IT NITJ'24"),
 ('Sakshi Warandani' , "" , "NITJ wish me on 23 jan"),
 ("chhaprinibba", "/sdfvsdf", "Heart Stealer"),
